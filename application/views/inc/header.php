9758def39223b8f8b9e473c143e3e0a51b84b8c6
hijax231 <jandeanpasajol231@gmail.com>
2019-09-20 20:19:22 +0800

    pylon international

--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pylon</title>
+    <title>Pylon International</title>
 </head>
 
 
@@ -23,7 +23,6 @@
   <a href="#home" id="logo"><img src="<?php echo base_url() .'/assets/img/logo.png'?>" height="80px" alt="" srcset=""></a>
   <div id="navbar-right" class="mt-4">
     <a class="active hidse" href="#home">Home</a>
-
     <a class="hidse"href="#services">Products and Serivice Offered</a>
     <a class="hidse"href="#services">List of Projects</a>
     <a class="hidse"href="#about">About Us</a>
