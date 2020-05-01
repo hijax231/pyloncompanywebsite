d2b926ee2d73e6fcc3001e4dc89bc529d84cf0e3
hijax231 <antonmarimar231@gmail.com>
2020-05-01 09:05:04 +0800

    hatdog

--- application/controllers/EmailController.php
+++ application/controllers/EmailController.php
@@ -27,46 +27,54 @@ class EmailController extends CI_Controller {
       ];
 
       echo json_encode($data);
-    }
-//SMTP & mail configuration
 
-// $config['protocol']    = 'smtp';
-// $config['smtp_host']    = 'smtp.gmail.com';
-// $config['smtp_port']    = '465';
-// $config['smtp_timeout'] = '7';
-// $config['smtp_user']    = 'pylondev10@gmail.com';
-// $config['smtp_pass']    = 'ITpylon@dmin10';
-// $config['charset']    = 'utf-8';
-// $config['newline']    = "\r\n";
-// $config['mailtype'] = 'text'; // or html
-// $config['validation'] = TRUE; // bool whether to validate email or not     
+// SMTP & mail configuration
 
-
-
-// $config = Array(
-//     'protocol' => 'mail',
-//     'smtp_host' => 'mail.pyloninternationals.com',
-//     'smtp_port' => 587 ,
-//     'smtp_user' => 'pylondev10@pyloninternationals.com',
-//     'smtp_pass' => 'ITpylon@dmin10',
-//     'mailtype'  => 'html', 
-//     'charset'   => 'iso-8859-1'
-// );
+$config['protocol']    = 'smtp';
+$config['smtp_host']    = 'smtp.gmail.com';
+$config['smtp_port']    = '465';
+$config['smtp_timeout'] = '7';
+$config['smtp_user']    = 'pylondev10@gmail.com';
+$config['smtp_pass']    = 'ITpylon@dmin10';
+$config['charset']    = 'utf-8';
+$config['newline']    = "\r\n";
+$config['mailtype'] = 'text'; // or html
+$config['validation'] = TRUE; // bool whether to validate email or not     
 
 
-// $this->load->library('email', $config);
-// $this->email->set_newline("\r\n");
+
+$config = Array(
+    'protocol' => 'mail',
+    'smtp_host' => 'mail.pyloninternationals.com',
+    'smtp_port' => 587 ,
+    'smtp_user' => 'pylondev10@pyloninternationals.com',
+    'smtp_pass' => 'ITpylon@dmin10',
+    'mailtype'  => 'html', 
+    'charset'   => 'iso-8859-1'
+);
+
+
+$this->load->library('email', $config);
+$this->email->set_newline("\r\n");
  
-// //Email content
-// $htmlContent = '<h1>Sending email via Gmail SMTP server</h1>';
-// $htmlContent .= '<p>This email has sent via Gmail SMTP server from CodeIgniter application.</p>';
+//Email content
+$htmlContent = '<p>'.$data['fullname'].'</p>';
+$htmlContent .= '<p>'.$data['address'].'</p><br>';
+$htmlContent .= '<p>'.$data['position'].'</p>';
+$htmlContent .= '<p>'.$data['contact'].'</p><br>';
+
+
+$htmlContent .= '<p>'.date("Y/m/d").'</p><br>';
+
+
+$htmlContent .= '<p>'.$data['message'].'</p><br>';
+
+$this->email->to('pasajol231@gmail.com');
+$this->email->from('pylondev10@pyloninternationals.com','Pylon International Trading Corp.');
+$this->email->subject('E-Qurantine Pass Inquiry');
+$this->email->message($htmlContent);
  
-// $this->email->to('pasajol231@gmail.com');
-// $this->email->from('pylondev10@pyloninternationals.com','Pylon International Trading Corp.');
-// $this->email->subject('How to send email via Gmail SMTP server in CodeIgniter');
-// $this->email->message($htmlContent);
- 
-// //Send email
-// $this->email->send();
-    
+//Send email
+$this->email->send();
+}
 }
